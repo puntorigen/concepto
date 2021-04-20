ed71db2aefe6a218fcd98c54bcc17c5da2679633
Pablo Schaffner <pablo@puntorigen.com>
2021-04-19 23:56:02 -0400

    - changed timer of commands

--- src/index.js
+++ src/index.js
@@ -906,9 +906,9 @@ export default class concepto {
 		// instead of marking and showing time, we want in vue to build a time table and show it with another method
 		if (arguments.length>0) {
 			let keys = {...arguments[0]};
-			if (typeof keys.id !== 'undefined' && keys.id.indexOf('def_')!=-1) { //&& keys.id.indexOf('_x')!=-1
+			if (typeof keys.id !== 'undefined') { //&& keys.id.indexOf('_x')!=-1
 				let filter_key = keys.id.split(' ')[0];
-				if (typeof this.x_time_stats.times[filter_key] === 'undefined') {
+				if (typeof this.x_time_stats.times[filter_key] === 'undefined' && filter_key.indexOf('def_')!=-1) {
 					this.x_time_stats.times[filter_key] = new Date();
 					this.x_time_stats.tables[filter_key] = { command:filter_key, calls:0, average_call:0, total_ms:0 };
 				}
@@ -932,7 +932,7 @@ export default class concepto {
 		if (arguments.length>0) { 
 			let keys = {...arguments[0]}, filter_key=''; // && keys.id.indexOf('_x')!=-1
 			if (typeof keys.id !== 'undefined') filter_key = keys.id.split(' ')[0];
-			if (typeof keys.id !== 'undefined' && keys.id.indexOf('def_')!=-1 && filter_key in this.x_time_stats.times) {
+			if (typeof keys.id !== 'undefined' && filter_key.indexOf('def_')!=-1 && filter_key in this.x_time_stats.times) {
 				//if (!this.x_time_stats.tables[keys.id]) this.x_time_stats.tables[keys.id] = {};
 				if (typeof this.x_time_stats.tables[filter_key] !== 'undefined') {
 					let timePassed = new Date().getTime() - this.x_time_stats.times[filter_key].getTime();
